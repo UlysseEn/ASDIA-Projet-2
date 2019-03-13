1ce2e6d0fabe0d560f1edf0f928ad3bf6e341dbd
Guillaume Caulier <Guillaume.Caulier@grenoble-inp.org>
2019-03-13 09:19:56 +0100

    pour que ulysse le naze puisse tester

--- src/swCalc.c
+++ src/swCalc.c
@@ -9,8 +9,8 @@
 struct matrix *swInitMat(char *s1, char *s2) {
     struct matrix *mat = NULL;
     mat = malloc(sizeof(struct matrix*));
-    mat->w = strlen(s1);
-    mat->h = strlen(s2);
+    mat->w = strlen(s1) + 1;
+    mat->h = strlen(s2) + 1;
     mat->cells = malloc(mat->w * mat->h * sizeof(struct cells*));
     for(size_t i=0; i <= mat->w; i++) {
       for(size_t j=0; j <= mat->h; j++) {
@@ -26,12 +26,15 @@ struct matrix *swInitMat(char *s1, char *s2) {
 }
 
 void swFillMat(struct matrix *mat, struct cost *cost, char *s1, char *s2) {
-  for(size_t i=1; i <= mat->w; i++) {
-    for(size_t j=1; j <= mat->h; j++) {
-      double score1 = mat->cells[w * (i-1) + j] + cost->indelOpen;
-      double score2 = mat->cells[w * i + j-1] + cost->indelOpen;
-      double score3 = mat->cells[w * (i-1) + j-1] + cost->subst(s1[i], s2[j])
-      mat->cells[w * i + j] = min(score1, score2, score3); //A coder
+  for(size_t i=1; i < mat->w; i++) {
+    for(size_t j=1; j < mat->h; j++) {
+      double score1 = mat->cells[mat->w * (i-1) + j].score + cost->indelOpen;
+      double score2 = mat->cells[mat->w * i + j-1].score + cost->indelOpen;
+      score1 = ((score1 < score2) ? score2 : score1);
+      double score3 = mat->cells[mat->w * (i-1) + j-1].score + cost->subst(s1[i-1], s2[j-1]);
+      score1 = ((score1 < score3) ? score3 : score1);
+      score1 = ((score1 < 0) ? 0 : score1);
+      mat->cells[mat->w * i + j].score = score1;
     }
   }
 }
@@ -43,8 +46,10 @@ void swPrintMat(struct matrix *mat) {
   printf("\n------- Ma Matrice -------- \n\n");
   for(size_t i=0; i < mat->w; i++) {
     for(size_t j=0; j < mat->h; j++) {
-      printf("cells[%d,%d] = %.0f\n", i, j, mat->cells[mat->w * i + j].score);
+      //printf("cells[%d,%d] = %.0f\n", i, j, mat->cells[mat->w * i + j].score);
+      printf("%.0f ", mat->cells[mat->w * i + j].score);
     }
+    printf("\n");
   }
 }
 
