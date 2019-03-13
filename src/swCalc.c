c8c0edb2f92dc9fbbb77286bd996acfb2d0c94a0
Guillaume Caulier <Guillaume.Caulier@grenoble-inp.org>
2019-03-13 10:21:55 +0100

    jtebz

--- src/swCalc.c
+++ src/swCalc.c
@@ -27,12 +27,12 @@ struct matrix *swInitMat(char *s1, char *s2) {
 }
 
 void swFillMat(struct matrix *mat, struct cost *cost, char *s1, char *s2) {
-  for(size_t i=1; i < mat->w; i++) {
-    for(size_t j=1; j < mat->h; j++) {
+  for(size_t i=1; i < mat->h; i++) {
+    for(size_t j=1; j < mat->w; j++) {
       double score1 = mat->cells[mat->w * (i-1) + j].score + cost->indelOpen;
       double score2 = mat->cells[mat->w * i + j-1].score + cost->indelOpen;
       score1 = ((score1 < score2) ? score2 : score1);
-      double score3 = mat->cells[mat->w * (i-1) + j-1].score + cost->subst(s1[i-1], s2[j-1]);
+      double score3 = mat->cells[mat->w * (i-1) + j-1].score + cost->subst(s1[j-1], s2[i-1]);
       score1 = ((score1 < score3) ? score3 : score1);
       score1 = ((score1 < 0) ? 0 : score1);
       mat->cells[mat->w * i + j].score = score1;
@@ -45,8 +45,8 @@ void swPrintMat(struct matrix *mat) {
   printf("mat->w = %d\n", mat->w);
   printf("mat->h = %d\n", mat->h);
   printf("\n------- Ma Matrice -------- \n\n");
-  for(size_t i=0; i < mat->w; i++) {
-    for(size_t j=0; j < mat->h; j++) {
+  for(size_t i=0; i < mat->h; i++) {
+    for(size_t j=0; j < mat->w; j++) {
       //printf("cells[%d,%d] = %.0f\n", i, j, mat->cells[mat->w * i + j].score);
       printf("%.0f ", mat->cells[mat->w * i + j].score);
     }
